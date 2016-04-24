0d287cb37e76a6826f1e843196bf6404d9d0f094
kotelnikov <m89817380346@gmail.com>
2016-04-24 20:28:12 +0300

    contacts and map

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,27 @@ gulp.task('build', function () {
 
 	gulp.src([
 			'bower_components/reset-css/reset.css',
+			'bower_components/bootstrap/dist/css/bootstrap.css',
+			'bower_components/font-awesome/css/font-awesome.css',
 			'bower_components/fullpage.js/dist/jquery.fullpage.css',
 			'css/main.css'
 		])
 	.pipe(concat('css/build.css'))
 	.pipe(cssnano())
 	.pipe(gulp.dest('./'))
+
+	gulp.src([
+			'bower_components/font-awesome/fonts/fontawesome-webfont.*',
+		])
+	.pipe(gulp.dest('fonts/'))
+});
+
+gulp.task('default', function(){
+	gulp.watch('css/main.css', function(){
+		gulp.run('build');
+	});
+
+	// gulp.watch('js/**', function(){
+	// 	gulp.run('build');
+	// });
 })
